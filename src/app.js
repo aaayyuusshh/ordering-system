0bab4edc915d1ad288483475d00209149b3fb75a
j-felicio <jfelicio@JFF-PC>
2023-12-03 18:08:14 -0700

    Added click event for all menu item redirection

--- src/app.js
+++ src/app.js
@@ -163,13 +163,48 @@ function displayFilteredDishes(filteredDishes) {
             dishDescription.textContent = dish.description;
 
             imageDiv.style.backgroundImage = "url("+dish.image+")";
-
             dishItem.style.display = "block";    
         }
     })
 }
 
-// Event listener for the specific dish item in menu.html
-document.querySelector('.imgDiv4').addEventListener('click', function() {
-    window.location.href = 'item.html';
-});
+let currentCategory = 'Popular'; // Default category
+
+function setupDishClickListeners() {
+    // Add listeners to category buttons
+    const categoryButtons = document.querySelectorAll('.dishCategoryButton');
+    categoryButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            currentCategory = button.textContent.trim();
+        });
+    });
+
+    // Add listeners to dish items
+    const imgDivs = document.querySelectorAll('.imgDiv');
+    imgDivs.forEach((imgDiv, index) => {
+        imgDiv.addEventListener('click', () => {
+            const offset = getIndexOffset(currentCategory);
+            const actualIndex = index + offset;
+
+            if (dishes[actualIndex]) {
+                const dishData = JSON.stringify(dishes[actualIndex]);
+                localStorage.setItem('selectedDish', dishData);
+                window.location.href = 'item.html';
+            }
+        });
+    });
+}
+
+function getIndexOffset(category) {
+    const categoryOrder = ['Popular', 'Appetizers', /* other categories... */];
+    let offset = 0;
+
+    for (let i = 0; i < categoryOrder.indexOf(category); i++) {
+        offset += dishes.filter(dish => dish.category === categoryOrder[i]).length;
+    }
+
+    return offset;
+}
+
+document.addEventListener('DOMContentLoaded', setupDishClickListeners);
+
