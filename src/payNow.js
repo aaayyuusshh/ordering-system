7d6b7f04e656f3588152fded5eeb1e1c0f0298d6
eugenioangelica <60020396+eugenioangelica@users.noreply.github.com>
2023-12-06 16:49:49 -0700

    added the get the bill button

---
+++ src/payNow.js
@@ -0,0 +1,39 @@
+function showBillConfirmation() {
+    // Reset bill confirmation modal
+    resetBillModal();
+
+    // Show bill confirmation modal
+    var billModal = document.getElementById("billConfirmationModal");
+    billModal.style.display = "block";
+}
+
+function closeBillModal() {
+    var billModal = document.getElementById("billConfirmationModal");
+    billModal.style.display = "none";
+}
+
+function confirmBill() {
+    var billModalContent = document.getElementById("billModalContent");
+    billModalContent.innerHTML = `
+    <div class='popUpMainContainer'>
+        <p class='modalText'>The waiter has been notified to assist you with payment and will be with you shortly.</p>
+        <img src='img/waiter-icon.png' alt='Bill Icon' class='billIcon'>
+         <button class='modalButton' onclick='closeBillModal()'>Okay</button>
+       
+    </div>
+    `;
+}
+
+function resetBillModal() {
+    var billModalContent = document.getElementById("billModalContent");
+    billModalContent.innerHTML = `
+    <div class='popUpMainContainer'>
+        <p class='modalText'>Ready to request the bill?</p>
+        <img src='img/waiter-icon.png' alt='Bill Icon' class='billIcon'>
+        <div class='decisionOptions'>
+            <button class='modalButton' onclick='confirmBill()'>Yes</button>
+            <button class='modalButton' onclick='closeBillModal()'>Cancel</button>
+        </div>
+    </div>
+    `;
+}
